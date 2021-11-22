fec4bc921b80ae3a4e79034d142642a1b9e378b1
Satyam Singh <satyamsingh20027@gmail.com>
2021-11-22 09:57:22 +0530

    Edits
    1) Our Team component created
    
    2) Changed the our revolution svg width dimensions.

---
+++ src/components/OurTeam.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+/* doc link-  https://docs.google.com/document/d/1g2vRm_g6phcWiybLyVKK_g_3J6U4dI_wEmJKbYkUGvg/edit */
+
+
+function OurTeam() {
+    return (<div>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+            <div>   Department of Education -
+                Director, Directorate of Education - Shri Hemant Biswas
+                Advisor to Directorate of Education - Shri Shailendra Sharma
+                State Program Head - Shri Praveen Chaudhary, OSD to Dy. Cm
+                2.   Our Partners
+                DTU - Implementation
+                Meet the Team
+                Head of the PMU - Dr A.K Shrivastava, PMU Members
+                Other members -  Shivani, Anubhooti, Rahul, Radhe Raman Singh
+                Project Head
+                Program Managers - Name, contact no, official id, constituency
+                Media team
+                B. Alohomora Education Foundation - Design & Capacity Building (logo)
+                C. Saarthi - Tech Development & Support (LOGO)
+                3. Our Participants:
+
+                Want to contribute - Join the team
+
+
+
+            </div>
+            <div>
+                Project Head - Ankur Sehgal - official email Id - ankursehgal@deshkementor.com
+                Program Manager:
+                Abhinay Singh - official email id - abhinaysingh@deshkementor.com, Constituency - Chandni Chowk
+                Arpit Sharma - official email Id-  arpit@deshkementor.com Constituency - East Delhi
+                Deendyal Singh - official email id -   deendyal@deshkementor.com Constituency - North -West
+                Ekta Gupta - official email id - ektagupta@deshkementor.com Constituency - West Delhi
+                Prerak Mittal - official email Id - prerak.mittal@deshkementor.com Constituency - North East
+                Ankit Jayant - ankitjayant@deshkementor.com Constituency - South Delhi
+                Tanuj Pratap Singh - tanujjadon@deshkementor.com Constituency - New Delhi
+                Social Media Team:
+                Lohit Lodhwal - lohitlodhwalfilms@gmail.com - Designation - Video Producer
+                Arisha Parwez - arisha.parvez@deshkementor.com - Designation - Social Media Assosciate</div>
+
+        </div>
+        <div style={{ display: 'grid', gridTemplateColumns: 'auto auto auto auto' }}>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+            <div>5</div>
+            <div>6</div>
+            <div>7</div>
+            <div>8</div>
+        </div>
+    </div>)
+}
+
+export default OurTeam
